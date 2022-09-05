9bfb039c5fbba437b3d22f8ada1b4b055a062b94
Pablo Tato Ramos <pabtatoramos@gmail.com>
2022-09-05 10:48:09 +0200

    Add test command

--- build.zig
+++ build.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-pub fn build(b: *std.build.Builder) void {
+pub fn build(b: *std.build.Builder) !void {
     const target = b.standardTargetOptions(.{});
     const mode = b.standardReleaseOptions();
 
@@ -14,5 +14,13 @@ pub fn build(b: *std.build.Builder) void {
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| run_cmd.addArgs(args);
 
+    var argv = std.ArrayList([]const u8).init(b.allocator);
+    try argv.append("toml-test.exe");
+    try argv.append("zig-out/bin/test-sometoml.exe");
+    if (b.args) |args| try argv.appendSlice(args);
+    const toml_test = b.addSystemCommand(argv.items);
+    toml_test.step.dependOn(b.getInstallStep());
+
     b.step("run", "Run the app").dependOn(&run_cmd.step);
+    b.step("test", "Test using BurntSushi/toml-test").dependOn(&toml_test.step);
 }
